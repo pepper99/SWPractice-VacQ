b660e674fa10c33dee220cebdae4d656daa32b09
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-01 20:39:11 +0700

    Using Express Router

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+//Route files
+const hospitals = require('./routes/hospitals');
+
 dotenv.config({path: './config/config.env'});
 
 const app = express();
 
-app.get('/api/v1/hospitals', (req, res) => {
-  res.status(200).json({success: true, msg: 'Show all hospitals'});
-})
-
-app.get('/api/v1/hospitals/:id', (req, res) => {
-  res.status(200).json({success: true, msg: `Show hospital ${req.params.id}`});
-})
-
-app.post('/api/v1/hospitals', (req, res) => {
-  res.status(200).json({success: true, msg: 'Create new hospitals'});
-})
-
-app.put('/api/v1/hospitals/:id', (req, res) => {
-  res.status(200).json({success: true, msg: `Update hospital ${req.params.id}`});
-})
-
-app.delete('/api/v1/hospitals/:id', (req, res) => {
-  res.status(200).json({success: true, msg: `Delete hospital ${req.params.id}`});
-})
+//Mount routers
+app.use('/api/v1/hospitals', hospitals);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
