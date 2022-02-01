1eb71c5ea783b07e55e7b00a0d555589ab3933f4
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-01 20:27:23 +0700

    Add all /api/v1/hospitals routes

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ dotenv.config({path: './config/config.env'});
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({success: true, data: {id: 1}});
+app.get('/api/v1/hospitals', (req, res) => {
+  res.status(200).json({success: true, msg: 'Show all hospitals'});
+})
+
+app.get('/api/v1/hospitals/:id', (req, res) => {
+  res.status(200).json({success: true, msg: `Show hospital ${req.params.id}`});
+})
+
+app.post('/api/v1/hospitals', (req, res) => {
+  res.status(200).json({success: true, msg: 'Create new hospitals'});
+})
+
+app.put('/api/v1/hospitals/:id', (req, res) => {
+  res.status(200).json({success: true, msg: `Update hospital ${req.params.id}`});
+})
+
+app.delete('/api/v1/hospitals/:id', (req, res) => {
+  res.status(200).json({success: true, msg: `Delete hospital ${req.params.id}`});
 })
 
 const PORT = process.env.PORT || 5000;
