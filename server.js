56a200e11258d0c0e2f987db569647097ee43119
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-09 22:17:16 +0700

    Setup mongoose

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 //Route files
-const hospitals = require('./routes/hospitals');
+const hospitals = require("./routes/hospitals");
 
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: "./config/config.env" });
+
+//Connect to database
+connectDB();
 
 const app = express();
 
 //Mount routers
-app.use('/api/v1/hospitals', hospitals);
+app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+})
