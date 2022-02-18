9d6044b798fa161f00e2e5abdad3d0644018c63e
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-18 15:30:28 +0700

    Implement POST /api/v1/auth/register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,22 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 //@desc     Register user
 //@route    POST /api/v1/auth/register
 //@access   Public
-exports.register = (req, res, next) => {
-  res.status(200).json({ success: true });
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    //Create user
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role,
+    });
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(400).json({ success: false });
+    console.log(err.stack);
+  }
 };
