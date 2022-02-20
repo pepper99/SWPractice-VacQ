abe00c6f268c5b3d4b3ac1b435657f7d98d1fa71
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-20 22:30:48 +0700

    Implement POST /api/v1/auth/login

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,3 +22,33 @@ exports.register = async (req, res, next) => {
     console.log(err.stack);
   }
 };
+
+//@desc     Login user
+//@route    POST /api/v1/auth/login
+//@access   Public
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Validate email & password
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide an email and password" });
+  }
+
+  //Check for user
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(400).json({ success: false, msg: "Invalid credentials" });
+  }
+
+  //Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return res.status(400).json({ success: false, msg: "Invalid credentials" });
+  }
+
+  //Create token
+  const token = user.getSignedJwtToken();
+  res.status(200).json({ success: true, token });
+};
