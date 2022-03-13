a5f15f14840d8eb84d57575ea78f18910da4ec6e
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-03-13 15:57:46 +0700

    GET /api/v1/hospitals advance filtering & pagination

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -4,12 +4,78 @@ const Hospital = require("../models/Hospital");
 //@route    GET /api/v1/hospitals
 //@access   Public
 exports.getHospitals = async (req, res, next) => {
+  let query;
+
+  //Copy req.query
+  const reqQuery = { ...req.query };
+  // console.log(reqQuery);
+
+  //Fields to exclude
+  const removeFields = ["select", "sort", "page", "limit"];
+
+  //Loop over remove fields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+  console.log(reqQuery);
+
+  //Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  //Create operators ($gt, $gte, etc)
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  // console.log(queryStr);
+
+  //finding resource
+  query = Hospital.find(JSON.parse(queryStr));
+
+  //select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+  //sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+  //pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Hospital.countDocuments();
+
+  console.log(page, limit);
+
+  query = query.skip(startIndex).limit(limit);
+
   try {
-    const hospitals = await Hospital.find();
+    const hospitals = await query;
+    //pagination result
+    const pagination = {};
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+    if (startIndex > 0) {
+      pagination.prev = { page: page - 1, limit };
+    }
     res
       .status(200)
-      .json({ success: true, count: hospitals.length, data: hospitals });
+      .json({
+        success: true,
+        count: hospitals.length,
+        pagination,
+        data: hospitals,
+      });
   } catch (err) {
+    console.log(err.stack);
     res.status(400).json({ success: false });
   }
 };
