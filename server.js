18abd97b293e52c7847fb08d91dda1d2a8df9ee9
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-03-29 20:21:44 +0700

    Implement /api/v1/auth/logout and backend protections

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 //Route files
 const hospitals = require("./routes/hospitals");
@@ -20,6 +26,25 @@ app.use(express.json());
 
 //Cookie parser
 app.use(cookieParser());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set secrutiy headers
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate limiting
+const limiter = rateLimit({ windowsMs: 10 * 60 * 1000, max: 100 });
+app.use(limiter);
+
+//Prevent http param pollutions
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
 
 //Mount routers
 app.use("/api/v1/hospitals", hospitals);
